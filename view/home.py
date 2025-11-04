69d09ed16a7a71f033bbb7f19d5e6f00c06cd366
Ytee08 <michaellamptey483@gmail.com>
2025-11-04 08:22:19 +0000

    Intial commit

---
+++ view/home.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+
+
+COLORS = {
+    "bg": "#1C2333",        
+    
+}
+
+root = tk.Tk()
+root.title("Quiz App")
+root.geometry("420x600")
+root.configure(bg=COLORS["bg"])
+root.resizable(False, False)
+
+
+
+
+
+
+root.mainloop()
